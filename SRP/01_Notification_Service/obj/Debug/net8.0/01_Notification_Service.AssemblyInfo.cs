f148d70b4dd5505748e3b3b052cdd5952f822772
Alae Azzaoui <125179905+AzzaouiAlae@users.noreply.github.com>
2024-11-23 17:28:21 +0100

    a

--- SRP/01_Notification_Service/obj/Debug/net8.0/01_Notification_Service.AssemblyInfo.cs
+++ SRP/01_Notification_Service/obj/Debug/net8.0/01_Notification_Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01_Notification_Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e276a592e12c3f33926df193d8f59dc0b6556a0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("01_Notification_Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01_Notification_Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
