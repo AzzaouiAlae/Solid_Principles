a549ee5d2628c66e9b341f1325f2bceb21b50ebb
Alae Azzaoui <125179905+AzzaouiAlae@users.noreply.github.com>
2024-11-24 16:48:07 +0100

    OCP

---
+++ Open_Closed_Principle_(OCP)/01_Notification_Service_OCP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_Notification_Service_OCP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
