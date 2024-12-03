bbaa2d80a043e5b72684f00e711b0879806ff7ba
Alae azzaoui <azzaoui@outlook.com>
2024-12-03 19:49:43 +0100

    DIP

--- Dependency_Inversion_Principle_(DIP)/01_Report_Violating_dip/Program.cs
+++ Dependency_Inversion_Principle_(DIP)/01_Report_Violating_dip/Program.cs
@@ -1,12 +1,32 @@
 ﻿namespace _01_Report_Violating_dip
 {
+    public class PdfReportGenerator
+    {
+        public void Generate()
+        {
+            Console.WriteLine("PDF report generated");
+        }
+    }
+    public class ReportGenerator
+    {
+        PdfReportGenerator _pdfReportGenerator;
+
+        public ReportGenerator()
+        {
+            _pdfReportGenerator = new PdfReportGenerator();
+        }
+        public void Generate()
+        {
+            _pdfReportGenerator.Generate();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            ReportGenerator reportGenerator= new ();
+            reportGenerator.Generate();
+
         }
     }
 }
-
-
---
+++ Dependency_Inversion_Principle_(DIP)/02_Report_DIP/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace _02_Report_DIP;
+
+public interface IGenerateReporte
+{
+    public void Generate();
+}
+public class WordReportGenerator : IGenerateReporte
+{
+    public void Generate()
+    {
+        Console.WriteLine("Word report generated");
+    }
+}
+public class ExcelReportGenerator : IGenerateReporte
+{
+    public void Generate()
+    {
+        Console.WriteLine("Excel report generated");
+    }
+}
+public class CrystalReportGenerator : IGenerateReporte
+{
+    public void Generate()
+    {
+        Console.WriteLine("Crystal report generated");
+    }
+}
+public class PdfReportGenerator : IGenerateReporte
+{
+    public void Generate()
+    {
+        Console.WriteLine("PDF report generated");
+    }
+}
+public class ReportGenerator
+{
+    IGenerateReporte _pdfReportGenerator;
+    public ReportGenerator(IGenerateReporte Generator)
+    {
+        _pdfReportGenerator = Generator;
+    }
+    public void Generate()
+    {
+        _pdfReportGenerator.Generate();
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        ReportGenerator reportGenerator= new (new PdfReportGenerator());
+        reportGenerator.Generate();
+
+        reportGenerator= new (new WordReportGenerator());
+        reportGenerator.Generate();
+
+        reportGenerator= new (new ExcelReportGenerator());
+        reportGenerator.Generate();
+
+        reportGenerator= new (new CrystalReportGenerator());
+        reportGenerator.Generate();
+    }
+}
